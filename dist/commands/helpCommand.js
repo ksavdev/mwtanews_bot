269ec043b1b0b190a2ffe30abfde16bce4b710ff
Kostia Savchenko <k.savchenko.dev@gmail.com>
2025-05-21 21:28:50 +0300

    Remove deprecated command files and related functionalities
    - Deleted command files: commandList.js, dailyNews.js, help.js, helpCommand.js, setLang.js, setNewsType.js, setTzCommand.js, utcHelp.js, weeklyNews.js.
    - Removed associated functions and logic for handling user commands related to news and settings.
    - Cleaned up database interaction files: db.js, user.service.js.
    - Removed unused services and utilities: teApi.js, log.js, scrape.js, dailyNewsScheduler.js, timezoneMenu.js, helpMessage.js.
    - Ensured all related functionalities are removed to streamline the codebase.

--- dist/commands/helpCommand.js
+++
@@ -1,7 +0,0 @@
-import { helpMessage } from "../messages/helpMessage.js";
-export const helpCommand = (bot) => {
-    bot.command("help", async (ctx) => {
-        ctx.reply(`Помощь
-${helpMessage}`);
-    });
-};
--- dist/commands/utcHelp.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.utcHelpCommand = void 0;
-const utcHelpCommand = (bot) => {
-    bot.command("utc_help", async (ctx) => {
-        await ctx.conversation.enter("utcHelpConversation");
-    });
-};
-exports.utcHelpCommand = utcHelpCommand;
